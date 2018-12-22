c1fc3639b7534ff5526f53b61e9f6c9577ad6b03
root <root@Alien-desktop.localdomain>
2018-12-22 10:49:08 +1030

    editing and reference changes in chapter 2

---
+++ openAll.bat
@@ -0,0 +1,17 @@
+rem Simple batch file (Windows script) to open all relevant files in regular thesis workflow
+cd "C:\git\honours-thesis"
+start main.tex
+start reference_library.bib
+start ref_lib.enl
+cd Chapters
+start Chapter1.tex
+start Chapter2.tex
+start Chapter3.tex
+start Chapter4.tex
+start Chapter5.tex
+start Chapter6.tex
+start Chapter7.tex
+start Chapter8.tex
+cd C:\Users\Alien\OneDrive\Uni\ENGR7700 Engineering thesis project
+start "" "Work logbook.docx"
+exit
