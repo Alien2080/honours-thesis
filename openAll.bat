40128d14ae5ec4207eb878b5503e856bd5514661
alien2080 <tom_aylen@hotmail.com>
2019-03-04 10:45:15 +1030

    litreview final changes, draft work of chapter 4, edits in chapter 3

--- openAll.bat
+++ openAll.bat
@@ -11,7 +11,6 @@ start Chapter4.tex
 start Chapter5.tex
 start Chapter6.tex
 start Chapter7.tex
-start Chapter8.tex
 cd C:\Users\Alien\OneDrive\Uni\ENGR7700 Engineering thesis project
 start "" "Work logbook.docx"
 exit
