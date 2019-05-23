c5085f6db4023c4e0cbb75b48c4bd7705a94bafa
alien2080 <tom_aylen@hotmail.com>
2019-05-23 21:15:40 +0930

    added Conclusion chapter and content. edits to several shapters

--- openAll.bat
+++ openAll.bat
@@ -2,7 +2,6 @@ rem Simple batch file (Windows script) to open all relevant files in regular the
 cd "C:\git\honours-thesis"
 start main.tex
 start reference_library.bib
-start ref_lib.enl
 cd Chapters
 start Chapter1.tex
 start Chapter2.tex
@@ -10,6 +9,5 @@ start Chapter3.tex
 start Chapter4.tex
 start Chapter5.tex
 start Chapter6.tex
-cd C:\Users\Alien\OneDrive\Uni\ENGR7700 Engineering thesis project
-start "" "Work logbook.docx"
+start Chapter7.tex
 exit
